bc853ba545aba5d07ee242f3e8568de2bd6d5048
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2025-04-05 03:38:14 +0700

    Update lotre.py

--- lotre.py
+++ lotre.py
@@ -8,8 +8,8 @@ def pick_unique_numbers(digit_count):
 
 # Interaktif: user pilih berapa digit
 try:
-    jumlah_digit = int(input("Mau ambil berapa digit (1-8)? "))
+    jumlah_digit = int(input("Mau ambil berapa digit (1-5)? "))
     hasil = pick_unique_numbers(jumlah_digit)
     print(f"Hasil ({jumlah_digit} digit): {', '.join(map(str, hasil))}")
 except ValueError as e:
-    print("Input tidak valid. Masukkan angka antara 1 sampai 8.")
+    print("Input tidak valid. Masukkan angka antara 1 sampai 5.")
